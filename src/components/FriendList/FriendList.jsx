0346a3041ea4a6cca8cd9c48eee82761de0b599c
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-06-16 08:36:00 +0300

    task-3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,7 @@ import FriendListItem from "../FriendListItem/FriendListItem";
 
 export default function FriendList({ friends }) {
   return (
-    <ul style={{ display: "flex", gap: "8px" }}>
+    <ul style={{ display: "flex", gap: "8px", marginBottom: "24px" }}>
       {friends.map(({ avatar, name, isOnline, id }) => {
         return (
           <li key={id}>
